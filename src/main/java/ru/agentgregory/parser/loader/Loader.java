ca84c84311616261bd4981edb7b843b367e7d9ab
Yaroslav Ryabov <yar.ryabov@mail.ru>
2022-05-10 20:20:47 +0400

    snapshot 1.0.1

--- src/main/java/ru/agentgregory/parser/loader/Loader.java
+++ src/main/java/ru/agentgregory/parser/loader/Loader.java
@@ -3,12 +3,18 @@ package ru.agentgregory.parser.loader;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import javax.security.auth.login.LoginException;
+
 public class Loader {
 
     private RestTemplate restTemplate = new RestTemplate();
 
     public String load(String url){
         ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        if (response.getStatusCode().isError()){
+            throw new ru.agentgregory.parser.loader.exception.LoadEx("bad status code"); //extends Exception
+
+        }
     return response.getBody();
     }
 
