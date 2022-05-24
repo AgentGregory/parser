9afc255a58ebc69eccd941388e073b66144a61fd
AgentGregory <grege2005@gmail.com>
2022-05-24 12:55:08 +0400

    1.0.4 SNAPSHOT

--- src/main/resources/db/migrations/V1__base_text.sql
+++ src/main/resources/db/migrations/V1__base_text.sql
@@ -11,5 +11,20 @@ CREATE TABLE articles
     username VARCHAR   NOT NUll ,
     url      VARCHAR   NOT NUll
     };
+CREATE TABLE categories
+(
+    id      BIGSERIAL  PRIMARY KEY,
+    name    VARCHAR    NOT NULL,
+    url     VARCHAR    NOT NULL
+);
+CREATE TABLE statistic
+(
+    id          BIGSERIAL PRIMARY KEY,
+    reputation  INTEGER,
+    views       INTEGER,
+    bookmarks   INTEGER,
+    comment     INTEGER,
+);
+
 
 
